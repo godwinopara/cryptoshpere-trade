20b9f37e4a19481195c0c294347165572e88da27
Godwin Opara <godwinopara62@yahoo.com>
2023-12-27 18:03:22 +0100

    fix complete

---
+++ src/config/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyDtp6qCdaxJDX-s14HoSQsJ2dKUt4EsBHc",
+	authDomain: "cryptosphere-trade.firebaseapp.com",
+	projectId: "cryptosphere-trade",
+	storageBucket: "cryptosphere-trade.appspot.com",
+	messagingSenderId: "543591832352",
+	appId: "1:543591832352:web:a46c673583497de57b1545",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
