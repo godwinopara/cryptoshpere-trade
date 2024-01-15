9ad372c417cfa39b69d2732bf38770a5dbdc36ca
Godwin Opara <godwinopara62@yahoo.com>
2024-01-15 15:21:23 +0100

    added background video

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -2,11 +2,12 @@
 
 import { TickerTape } from "react-ts-tradingview-widgets";
 import Button from "../shared/Button";
+import VideoBackground from "../shared/VideoBackground";
 
 export default function Hero() {
 	return (
 		<>
-			<div className="bg-black bg-pattern min-h-[85svh] md:min-h-[50svh] xl:min-h-[90vh] flex items-center  bg-right text-white1 pt-10 xl:pb-10">
+			{/* <div className="bg-black bg-pattern min-h-[85svh] md:min-h-[50svh] xl:min-h-[90vh] flex items-center  bg-right text-white1 pt-10 xl:pb-10">
 				<div className="px-6 xl:px-0 grid text-center lg:text-left lg:grid-cols-2 lg:gap-20 items-center max-w-desktop mx-auto">
 					<div className="mb-20 lg:mb-0">
 						<div className="mb-10">
@@ -25,18 +26,32 @@ export default function Hero() {
 					</div>
 					<div>
 						<video autoPlay loop>
-							<source
-								src="https://static.tradingview.com/static/bundles/chart-big.fb88521d9cf80be23da7.webm"
-								type="video/webm"
-							/>
-							<source
-								src="https://static.tradingview.com/static/bundles/chart-big.avc1.e7da14709759f81c9e4b.mp4"
-								type="video/webm;codecs=avc1"
-							/>
+							<source src="./video/bg-video.mp4" type="video/mp4" />
 						</video>
 					</div>
 				</div>
-			</div>
+			</div> */}
+			<VideoBackground>
+				<div className=" min-h-[85svh] md:min-h-[50svh] xl:min-h-[93vh] flex items-center  bg-right text-white1 pt-10 xl:pb-10">
+					<div className="px-6 xl:px-0 grid text-center lg:text-left lg:flex lg:gap-20 items-center max-w-desktop mx-auto">
+						<div className="mb-20 lg:mb-0 w-[55%]">
+							<div className="mb-10">
+								<h1 className="text-4xl  lg:text-90 lg:leading-120  mb-5">
+									Digital assets, investments and trading
+								</h1>
+								<p className="text-lg mb-4 xl:text-xl">
+									Join millions of users, from beginners to professionals, who use Universal
+									Cryptosphere Trade to purchase crypto, trade, and manage their investments in a
+									smarter way.
+								</p>
+							</div>
+							<div className="lg:flex items-center gap-2">
+								<Button btnText="Create Account" className="bg-green" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</VideoBackground>
 			<TickerTape colorTheme="light"></TickerTape>
 		</>
 	);
---
+++ src/components/shared/VideoBackground.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export default function VideoBackground({ children }: { children: ReactNode }) {
+	return (
+		<div className="relative">
+			<video autoPlay loop muted className="w-full h-full object-cover absolute top-0 left-0 z-0">
+				<source src="./video/bg-video.mp4" type="video/mp4" />
+			</video>
+			<div className="z-10 relative">{children}</div>
+		</div>
+	);
+}
