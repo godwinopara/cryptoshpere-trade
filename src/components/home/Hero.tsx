70f1ac8a8ff4a50a782a5cae52e2d1c3dc76aab2
Godwin Opara <godwinopara62@yahoo.com>
2024-01-16 22:30:26 +0100

    video play on mobile

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -7,31 +7,6 @@ import VideoBackground from "../shared/VideoBackground";
 export default function Hero() {
 	return (
 		<>
-			{/* <div className="bg-black bg-pattern min-h-[85svh] md:min-h-[50svh] xl:min-h-[90vh] flex items-center  bg-right text-white1 pt-10 xl:pb-10">
-				<div className="px-6 xl:px-0 grid text-center lg:text-left lg:grid-cols-2 lg:gap-20 items-center max-w-desktop mx-auto">
-					<div className="mb-20 lg:mb-0">
-						<div className="mb-10">
-							<h1 className="text-4xl  lg:text-90 lg:leading-120  mb-5">
-								Digital assets, investments and trading
-							</h1>
-							<p className="text-lg mb-4 xl:text-xl">
-								Join millions of users, from beginners to professionals, who use Universal
-								Cryptosphere Trade to purchase crypto, trade, and manage their investments in a
-								smarter way.
-							</p>
-						</div>
-						<div className="lg:flex items-center gap-2">
-							<Button btnText="Create Account" className="bg-green" />
-						</div>
-					</div>
-					<div>
-						<video autoPlay loop>
-							<source src="./video/bg-video.mp4" type="video/mp4" />
-						</video>
-					</div>
-				</div>
-			</div> */}
-
 			<div className="relative xl:min-h-[90vh]">
 				<VideoBackground>
 					<div className="min-h-[85vh] md:min-h-[50svh] xl:min-h-[95vh] flex items-center  bg-right text-white1 pt-10 xl:pb-10">
--- src/components/shared/VideoBackground.tsx
+++ src/components/shared/VideoBackground.tsx
@@ -1,9 +1,36 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 
 export default function VideoBackground({ children }: { children: ReactNode }) {
+	const videoRef = useRef<HTMLVideoElement | null>(null);
+
+	useEffect(() => {
+		const video = videoRef.current;
+		const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+			navigator.userAgent
+		);
+
+		if (video && isMobile) {
+			video.pause();
+		}
+
+		const handleClick = () => {
+			if (video?.paused) {
+				video.play();
+			}
+		};
+
+		document.addEventListener("click", handleClick);
+
+		return () => {
+			document.removeEventListener("click", handleClick);
+		};
+	}, []);
+
 	return (
 		<div className="relative w-full h-full">
 			<video
+				ref={videoRef}
+				playsInline
 				autoPlay
 				loop
 				muted
