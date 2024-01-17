b35dc1e3239e5d60fe1a0e5da7f2701f18042e32
Godwin Opara <godwinopara62@yahoo.com>
2024-01-17 04:09:42 +0100

    update description

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,35 +10,17 @@ import WhyChooseUs from "@/components/home/WhyChooseUs";
 import WhyTradeCrypto from "@/components/home/WhyTradeCrypto";
 import Head from "next/head";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Trade global CFD markets. Forex, Indices &amp; more. Universal Cryptosphere Trade",
+	description:
+		"Forex Broker offering Forex, Indices, Commodities and Share CFDs. Powerful platforms, trading resources and more.",
+};
+
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title>
-					Trade global CFD markets. Forex, Indices &amp; more. Universal Cryptosphere Trade.
-				</title>
-				<meta
-					name="description"
-					content="Forex Broker offering Forex, Indices, Commodities and Share CFDs. Powerful platforms, trading resources and more."
-				/>
-
-				<link rel="canonical" href="index.html" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:type" content="website" />
-				<meta
-					property="og:title"
-					content="Trade global CFD markets. Forex, Indices &amp; more. Universal Cryptosphere Trade."
-				/>
-				<meta
-					property="og:description"
-					content="Forex Broker offering Forex, Indices, Commodities and Share CFDs. Powerful platforms, trading resources, demo accounts and more."
-				/>
-				<meta property="og:url" content="https://universalcryptosphere.com" />
-				<meta property="og:site_name" content="Universal Cryptosphere Trade" />
-				<meta property="article:modified_time" content="2022-07-03T23:06:42+00:00" />
-				<meta name="twitter:card" content="summary_large_image" />
-				{/* Add other meta tags or links as needed */}
-			</Head>
 			<Hero />
 			<WhyChooseUs />
 			<Features />
